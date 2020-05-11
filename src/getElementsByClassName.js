60ab8f49f56b17046c6417fbbd07f559d84d77f4
cscottski <seescottski@yahoo.com>
2020-05-11 12:34:17 -0700

    Fixes test spec and outlines sudo code for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,13 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  // setup an array, for results
+  node = node || document.body;
+  var classNameArr = node.classList.split(' ');
+// check document.body for className
+// if find className store element in results array
+  // node.classList
+// for each child of current element... push getElements(className, child) into results array
+// return results array
 };
