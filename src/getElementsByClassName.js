3bfbba8f54e27be86f4d8b91efdfc08b62ba87d7
cscottski <seescottski@yahoo.com>
2020-05-11 15:22:47 -0700

    Adds checking for className within node.classList

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,10 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className, node) {
   // setup an array, for results
+  var results = [];
   node = node || document.body;
   var classNameArr = node.classList.split(' ');
 // check document.body for className
 // if find className store element in results array
+  for (let i = 0; i < classNameArr.length; i++) {
+    if (classNameArr[i] === className) {
+      results.push(node);
+    }
+  }
   // node.classList
 // for each child of current element... push getElements(className, child) into results array
 // return results array
